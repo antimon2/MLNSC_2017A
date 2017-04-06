bec19ab668563b7cddb85b7912a462c42336283c
antimon2 <antimon2.me@gmail.com>
2017-04-06 23:30:31 +0900

    Add Windows .bat and .ps1 files.

---
+++ docker.run.ps1
@@ -0,0 +1,2 @@
+$pwd_as_linux = "/$((pwd).Drive.Name.ToLowerInvariant())/$((pwd).Path.Replace('\', '/').Substring(3))"
+docker run -it --rm -v "${pwd_as_linux}:/opt/notebooks/mlnsc" -p 8888:8888 antimon2/mlnsc-dlscratch:latest
