25b9d811ef0563d5d74924d04537e4ee0fde3791
antimon2 <antimon2.me@gmail.com>
2017-05-19 17:11:51 +0900

    Fix docker.run.jupyter.sh (modified to match docker image update).

--- docker.run.jupyter.ps1
+++ docker.run.jupyter.ps1
@@ -1,2 +1,2 @@
 $pwd_as_linux = "/$((pwd).Drive.Name.ToLowerInvariant())/$((pwd).Path.Replace('\', '/').Substring(3))"
-docker run -it --rm -v "${pwd_as_linux}:/opt/notebooks/mlnsc" -p 8888:8888 antimon2/mlnsc-dlscratch:latest jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --NotebookApp.password='' --NotebookApp.token='' --NotebookApp.password_required=False
+docker run -it --rm -v "${pwd_as_linux}:/opt/notebooks/mlnsc" -p 8888:8888 antimon2/mlnsc-dlscratch:latest jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --NotebookApp.password='' --NotebookApp.token='' --NotebookApp.password_required=False --allow-root
